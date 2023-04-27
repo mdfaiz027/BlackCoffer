2c7db21c633b97537dedaccb9c53aff56ea66cf5
mdfaiz-techolution <mohammad.faiz@techolution.com>
2023-04-27 23:33:20 +0530

    added bottom navigation bar in the mainactivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,36 +1,49 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Get the FragmentManager
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        // Begin a new FragmentTransaction
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
 
-        /*// Instantiate your Fragment
-        RefineFragment myFragment = new RefineFragment();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        // Add the Fragment to the container view with an ID of "fragment_container"
-        fragmentTransaction.add(R.id.fragment_container, myFragment);*/
-
-        ExploreFragment exploreFragment = new ExploreFragment();
-
-        fragmentTransaction.add(R.id.fragment_container, exploreFragment);
-
-        // Commit the FragmentTransaction
-        fragmentTransaction.commit();
-
+                if(item.getItemId() == R.id.refine){
+                    navController.navigate(R.id.refineFragment);
+                }else if(item.getItemId() == R.id.explore){
+                    navController.navigate(R.id.exploreFragment);
+                }else if(item.getItemId() == R.id.network){
+                    //TODO
+                }else if(item.getItemId() == R.id.chat){
+                    //TODO
+                }else if(item.getItemId() == R.id.contacts){
+                    //TODO
+                }
+                return true;
+            }
+        });
     }
 }
